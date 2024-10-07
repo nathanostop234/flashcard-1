a46b3aa9146db69dc5e032d47633cb0dd1e4fdc0
github_user7801 <163008161+nathanremoslinsomanicos@users.noreply.github.com>
2024-10-07 11:18:47 -0300

    j

---
+++ perguntas.js
@@ -0,0 +1,44 @@
+CriaCartao(
+    'Programação 2.0' ,
+    'o que é phyton?' ,
+    'O phyton é uma linguagem de programação'
+)
+
+CriaCartao(
+    'Titulo 1' ,
+    'O Nathan tem problemas mentais?' ,
+    'Sim, o Nathan tem muitos problemas mentais.'
+)
+
+CriaCartao(
+    'Titulo 2' ,
+    'qual a capital da Irlanda?' ,
+    'Dublin'
+)
+
+CriaCartao(
+    'Titulo 3' ,
+    'Que horas são agora?' ,
+    '11:15'
+)
+
+CriaCartao(
+    'Processo' ,
+    'Porque o Wessly quer processar o Nathan?' ,
+    'Porquer o Nathan enche o saco todas a aulas'
+)
+
+CriaCartao(
+    'Titulo 6' ,
+    'O que a vaca bebe?' ,
+    'LEITE'
+)
+
+
+
+
+
+
+
+
+
