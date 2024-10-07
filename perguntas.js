5d3dd274e33043c8da4a1f02eba1fff4f3dd1af9
github_user7801 <163008161+nathanremoslinsomanicos@users.noreply.github.com>
2024-10-07 14:24:27 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,37 +1,43 @@
-CriaCartao(
-    'Programação 2.0' ,
-    'o que é phyton?' ,
-    'O phyton é uma linguagem de programação'
+CriaCartao ( 
+    'Curiosidades sobre o espaço', 
+    'Qual é o maior planeta do Sistema Solar?', 
+    'Júpiter' 
 )
 
-CriaCartao(
-    'Titulo 1' ,
-    'O Nathan tem problemas mentais?' ,
-    'Sim, o Nathan tem muitos problemas mentais.'
+CriaCartao ( 
+    'História do Brasil', 
+    'Em que ano foi proclamada a independência do Brasil?', 
+    '1822' 
 )
 
-CriaCartao(
-    'Titulo 2' ,
-    'qual a capital da Irlanda?' ,
-    'Dublin'
+CriaCartao (
+    'Processo',
+    'Porque o professor Weslly quer processar o Nathan?',
+    'Porquer o Nathan enche o saco dele todas as aulas'
 )
 
-CriaCartao(
-    'Titulo 3' ,
-    'Que horas são agora?' ,
-    '11:15'
+CriaCartao ( 
+    'Ciência', 
+    'Qual é o elemento químico mais abundante na atmosfera terrestre?', 
+    'Nitrogênio' 
 )
 
-CriaCartao(
-    'Processo' ,
-    'Porque o Wessly quer processar o Nathan?' ,
-    'Porquer o Nathan enche o saco todas a aulas'
+CriaCartao ( 
+    'Geografia', 
+    'Qual é o país mais populoso do mundo?', 
+    'China' 
 )
 
-CriaCartao(
-    'Titulo 6' ,
-    'O que a vaca bebe?' ,
-    'LEITE'
+CriaCartao ( 
+    'Matemática', 
+    'Qual é o valor de π (pi) arredondado para duas casas decimais?', 
+    '3.14' 
+)
+
+CriaCartao ( 
+    'Esportes', 
+    'Qual país venceu a Copa do Mundo de futebol em 1978?', 
+    'Argentina' 
 )
 
 
