4e5aef8f800c75825b85d768a3ecc6cd0efc22cb
github_user7801 <163008161+nathanremoslinsomanicos@users.noreply.github.com>
2024-10-07 11:33:42 -0300

    bhg

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ function CriaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
 }
+
+cartao.innerHTML = 
+<div class="cartao__conteudo">
+            <h3>Programação</h3>
+            <div class="cartao__conteudo__pergunta">
+                <p>O que é JavaScript</p>
+            </div>
+            <div class="cartao__conteudo__resposta">
+                <p>JavaScript é uma linguagem de programação</p>
+            </div>
+            </div>
